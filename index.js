6528be467bdb763e06a3cc62d9c0748800e89778
Ronil Patel <ronilpatel1001@gmail.com>
2023-07-25 11:02:50 -0400

    new all

--- index.js
+++ index.js
@@ -9,15 +9,78 @@ fetch("http://localhost:3000/workouts")
 // -create form to record workout circuit
 
 document.querySelector("#logo").addEventListener("click", function() {
-    const logo = document.getElementById("logo");
+    const logo = document.querySelector("#logo");
     logo.classList.add("spin-animation");
     setTimeout(() => {
       logo.classList.remove("spin-animation");
     }, 3000); 
   });
 
-  // EVENT LISTENER HOOKS UP ROLL DIE UP WITH GENERATOR
-document.querySelector(`#logo`).addEventListener(`click`, () => {
+//   // EVENT LISTENER HOOKS UP ROLL DIE UP WITH GENERATOR
+// document.querySelector(`#logo`).addEventListener(`click`, () => {
+//     // GRABS WORKOUT DATABASE AND GENERATES A RANDOM WORKOUT PROPERTY FROM THE ARRAY
+//     fetch("http://localhost:3000/workouts")
+//     .then(res => res.json())
+//     .then(workOuts => getRandomWorkout(workOuts))
+//     const getRandomWorkout = (workOuts) => { 
+//         if (!Array.isArray(workOuts) || workOuts.length === 0) {
+//             return null;
+//         }
+//         const randomIndex = Math.floor(Math.random() * workOuts.length);
+//         const generatedWorkOut = workOuts[randomIndex];
+//         const circuitList = document.querySelector(`#current-circuit`)
+//         const newP = document.createElement(`li`)
+//         const workoutImg = document.createElement(`img`)
+//         workoutImg.style.height = `200px`
+//         workoutImg.src = generatedWorkOut.img
+//         circuitList.append(workoutImg)
+//         newP.innerText = generatedWorkOut.workout
+//         newP.setAttribute(`class`, `current`)
+//         circuitList.appendChild(newP)
+//         const imgSpan = document.createElement(`span`)
+//         imgSpan.append(workoutImg)
+//         newP.append(imgSpan)
+//     }
+    
+//     })
+    
+    
+//     // CLEARS CURRENT CIRCUIT BUTTON
+//     const circuitList = document.body.querySelector(`#current-circuit`)
+//     const clearButton = document.body.querySelector(`#clear-circuit`)
+//     clearButton.addEventListener(`click`, () => {
+//         console.log(`i am cleared!`)
+//         circuitList.innerText = ``
+//     })
+    
+    
+    
+//     // ************CIRCUIT APPENDS, FIGUIRE OUT HOW TO GET SAVED CIRCUIT TO LIVE IN ITS OWN DIV
+//     const archiveButton = document.querySelector(`#save-circuit`)
+//     archiveButton.addEventListener(`click`, () => {
+//         console.log(`i am archived!`)
+//         const circuitHistory = document.querySelector(`#circuit-history`)
+//         const allWorkouts = document.querySelectorAll(`.current`)
+        
+//     // LOOP THROUGH EACH ELEMENT IN NODE LIST AND APPEND TO CIRCUIT HISTORY
+//         allWorkouts.forEach(workout => {
+//             const oneCircuit = document.createElement(`ul`)
+//             console.log(workout)
+//             oneCircuit.appendChild(workout.cloneNode(true));
+//             circuitHistory.appendChild(oneCircuit)
+//             circuitList.innerText = ``
+//             let btn = document.getElementById('btn');
+//             btn.addEventListener("click", function() {
+        
+//             }, {once : true});
+//        }) 
+
+       
+//     })    
+
+
+// EVENT LISTENER HOOKS UP ROLL DIE UP WITH GENERATOR
+document.querySelector(`#logo`).addEventListener(`click`, (e) => {
     // GRABS WORKOUT DATABASE AND GENERATES A RANDOM WORKOUT PROPERTY FROM THE ARRAY
     fetch("http://localhost:3000/workouts")
     .then(res => res.json())
@@ -33,14 +96,18 @@ document.querySelector(`#logo`).addEventListener(`click`, () => {
         const workoutImg = document.createElement(`img`)
         workoutImg.style.height = `200px`
         workoutImg.src = generatedWorkOut.img
-        circuitList.append(workoutImg)
         newP.innerText = generatedWorkOut.workout
         newP.setAttribute(`class`, `current`)
         circuitList.appendChild(newP)
-        
-    }
+        const imgSpan = document.createElement(`span`)
+        imgSpan.append(workoutImg)
+        newP.append(imgSpan)
+    // SET TIMEOUT WORKS, MAKE SURE THE TIMEOUT IS SET TO 8000 BY DEFAULT WHEN PRESENTING
+        setTimeout(() => {
+            workoutImg.remove();
+        }, 5000);  
+        }})
     
-    })
     
     
     // CLEARS CURRENT CIRCUIT BUTTON
@@ -53,25 +120,21 @@ document.querySelector(`#logo`).addEventListener(`click`, () => {
     
     
     
-    // ************CIRCUIT APPENDS, FIGUIRE OUT HOW TO GET SAVED CIRCUIT TO LIVE IN ITS OWN DIV
+    //CIRCUIT IS ARCHIVED IN ITS OWN DIV BELOW
     const archiveButton = document.querySelector(`#save-circuit`)
     archiveButton.addEventListener(`click`, () => {
         console.log(`i am archived!`)
         const circuitHistory = document.querySelector(`#circuit-history`)
-        const allWorkouts = document.querySelectorAll(`.current`)
-        
-    // LOOP THROUGH EACH ELEMENT IN NODE LIST AND APPEND TO CIRCUIT HISTORY
+        const allWorkouts = document.querySelectorAll(`#current-circuit .current`)
+        // LOOP THROUGH EACH ELEMENT IN NODE LIST AND APPEND TO CIRCUIT HISTORY
+        const archiveDiv = document.createElement(`div`)
+        archiveDiv.style.borderStyle = `outset`
         allWorkouts.forEach(workout => {
             const oneCircuit = document.createElement(`ul`)
             console.log(workout)
+            archiveDiv.appendChild(oneCircuit)
             oneCircuit.appendChild(workout.cloneNode(true));
-            circuitHistory.appendChild(oneCircuit)
-            circuitList.innerText = ``
-            let btn = document.getElementById('btn');
-            btn.addEventListener("click", function() {
-        
-            }, {once : true});
-       }) 
-
-       
-    })    
+            circuitHistory.append(archiveDiv)
+            circuitList.textContent = `` 
+        })
+    })
